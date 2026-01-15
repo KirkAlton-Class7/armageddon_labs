976bbd4fb0925778ee9a8c683301955cad48ba64
KirkAlton-Class7 <232495916+KirkAlton-Class7@users.noreply.github.com>
2026-01-14 22:43:41 -0600

    updated via CLI 2026-01-14 22:43:41 -0600

--- lab_1a/lab_1a_tf/999-key-pair.tf
+++ lab_1a/lab_1a_tf/999-key-pair.tf
@@ -1,5 +1,6 @@
-# # Register Public Key with AWS
 # # Uncomment and update with your local key information to register your key
+
+# # Register Public Key with AWS
 # resource "aws_key_pair" "tf_armageddon_key" {
 #   key_name   = "tf-armageddon-key"
 #   public_key = file("~/.ssh/tf-armageddon-key.pub")
