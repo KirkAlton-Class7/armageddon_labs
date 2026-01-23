590d59408abde1bfcde5d3733a98df749291db1a
KirkAlton-Class7 <232495916+KirkAlton-Class7@users.noreply.github.com>
2026-01-22 21:11:33 -0600

    updated via CLI 2026-01-22 21:11:33 -0600

--- lab_1c/15-route.tf
+++ lab_1c/15-route.tf
@@ -71,6 +71,12 @@ resource "aws_route_table" "local" {
 
 
 # Local Route Table Associations
+# S3 VPC Endpoint Route Table Associations
+resource "aws_vpc_endpoint_route_table_association" "s3_private_app_a" {
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+  route_table_id  = aws_route_table.local.id
+}
+
 # Private App Subnets
 resource "aws_route_table_association" "private_app_a" {
   subnet_id      = aws_subnet.private_app_a.id
@@ -86,6 +92,7 @@ resource "aws_route_table_association" "private_app_c" {
   subnet_id      = aws_subnet.private_app_c.id
   route_table_id = aws_route_table.local.id
 }
+
 
 # Private Data Subnets
 resource "aws_route_table_association" "private_data_a" {
