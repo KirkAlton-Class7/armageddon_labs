582bca69524dcf2fdd3fc4ebae0c272dd44d8186
KirkAlton-Class7 <232495916+KirkAlton-Class7@users.noreply.github.com>
2026-03-05 09:06:47 -0600

    updated via CLI 2026-03-05 09:06:47 -0600

--- lab_3_modular copy/modules/edge_dns_cdn/cloudfront_origin_auth.tf
+++
@@ -1,9 +0,0 @@
-# ================================================================
-# EDGE — CloudFront Origin Authentication
-# ================================================================
-
-# Secret header value used for CloudFront → ALB verification
-resource "random_password" "edge_auth_value" {
-  length  = 32
-  special = false
-}
